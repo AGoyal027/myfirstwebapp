c1adf016b14fcba6b452eb3715a804e7fb482bb3
AGoyal027 <ayushgoyal027@gmail.com>
2026-04-24 01:08:19 +0530

    Step 20: Adding a New Todo - Create a new View

--- README.md
+++ README.md
@@ -30,6 +30,7 @@ by Ranga Karanam (in28minutes) on Udemy.
 | Step 17 | Adding JSTL to Spring Boot Project & Showing Todos in Table           |
 | Step 18 | Adding Bootstrap CSS framework to Spring Boot Project using webjars   |
 | Step 19 | Formatting JSP pages with Bootstrap CSS framework                     |
+| Step 20 | Lets Add a New Todo - Create a new View                               |
 
 ## Connect
 - LinkedIn: https://linkedin.com/in/-ayush-goyal
--- src/main/java/com/goyal/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/goyal/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -19,10 +20,20 @@ public class TodoController {
 	}
 
 	@RequestMapping("list-todos")
-	public String ListToodos(ModelMap model){
+	public String listTodos(ModelMap model){
 		List<Todo> todo = todoService.findByUsername("goyal");
 		model.addAttribute("todos", todo);
 		return "listTodos";
+	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.GET)
+	public String showNewTodoPage(){
+		return "todo";
+	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.POST)
+	public String addNewTodo(){
+		return "redirect:list-todos";
 	}
 
 }
